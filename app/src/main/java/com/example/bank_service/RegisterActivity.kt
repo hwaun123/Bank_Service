df9840fef014161d166b4fd412df700ca5324362
hwaun123 <67179490+hwaun123@users.noreply.github.com>
2021-10-08 22:48:51 +0900

    [Add] Register data
    Register data

--- app/src/main/java/com/example/bank_service/RegisterActivity.kt
+++ app/src/main/java/com/example/bank_service/RegisterActivity.kt
@@ -2,6 +2,7 @@ package com.example.bank_service
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.ImageView
 
 class RegisterActivity : AppCompatActivity() {
@@ -11,8 +12,22 @@ class RegisterActivity : AppCompatActivity() {
 
         val ivBack:ImageView = findViewById(R.id.iv_register_back)
 
+        val editRegisterId: EditText = findViewById(R.id.edit_register_id)
+        val editRegisterPw: EditText = findViewById(R.id.edit_register_pw)
+        val editRegisterPN: EditText = findViewById(R.id.edit_register_phonenum)
+        val editRegisterName: EditText = findViewById(R.id.edit_register_name)
+        val editRegisterBirth: EditText = findViewById(R.id.edit_register_birth)
+
         ivBack.setOnClickListener {
             finish()
         }
+
+        var userId = editRegisterId.text.toString()
+        var userPw = editRegisterPw.text.toString()
+        var userPN = editRegisterPN.text.toString()
+        var userName = editRegisterName.text.toString()
+        var userBirth = editRegisterBirth.text.toString()
+
+        
     }
 }
