c6c271424c7e8bbe1d40aa8390ade6237f053455
hwaun123 <67179490+hwaun123@users.noreply.github.com>
2021-10-15 10:24:48 +0900

    [Add] Login function
    Login function

--- app/src/main/java/com/example/bank_service/LoginDialog.kt
+++ app/src/main/java/com/example/bank_service/LoginDialog.kt
@@ -2,11 +2,21 @@ package com.example.bank_service
 
 import android.app.Dialog
 import android.content.Context
+import android.content.Intent
+import android.util.Log
 import android.view.WindowManager
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.DialogFragment
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-class LoginDialog(context: Context)
+class LoginDialog(private val context: Context)
 {
     private val dialog = Dialog(context)
     private lateinit var onClickListener: OnDialogClickListener
@@ -24,12 +34,38 @@ class LoginDialog(context: Context)
         dialog.setCancelable(true)
         dialog.show()
 
+        val retrofit = Retrofit.Builder() // 레트로핏 연결
+            .baseUrl("http://34.64.165.105:8080/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val service = retrofit.create(RetrofitService::class.java)
+
         val btnLoginConfirm: Button = dialog.findViewById(R.id.btn_login_confirm)
         btnLoginConfirm.setOnClickListener{
             val id = dialog.findViewById<EditText>(R.id.edit_login_id).text.toString()
             val pw = dialog.findViewById<EditText>(R.id.edit_login_pw).text.toString()
 
+            service.login(Login(id,pw)).enqueue(object : Callback<Any?>{
+                override fun onResponse(call: Call<Any?>, response: Response<Any?>) {
+                    val code = response.code()
+                    Log.d("LoginRetrofit","code = $code")
+                    if(code == 200) {
+                        Toast.makeText(context,"로그인 성공!",Toast.LENGTH_SHORT).show()
+                        val intent = Intent(context,MainActivity::class.java)
+                        context.startActivity(intent)
 
+                    }
+                    else if (code == 401){
+                        Toast.makeText(context,"아이디나 비밀번호를 다시 확인해 주세요",Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+
+                override fun onFailure(call: Call<Any?>, t: Throwable) {
+                    Log.d("LoginRetrofit","연결 실패")
+                }
+            })
         }
 
     }
