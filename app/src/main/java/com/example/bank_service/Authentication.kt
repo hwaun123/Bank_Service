47cffe2ec9a319ab17a0032f8810e9374b5b4663
hwaun123 <67179490+hwaun123@users.noreply.github.com>
2021-10-18 18:28:45 +0900

    [Add] Authentication Class
    Authentication Class

---
+++ app/src/main/java/com/example/bank_service/Authentication.kt
@@ -0,0 +1,5 @@
+package com.example.bank_service
+
+data class Authentication(
+    var authentication: List<AuthenticationObject>? = null
+)
---
+++ app/src/main/java/com/example/bank_service/AuthenticationObject.kt
@@ -0,0 +1,8 @@
+package com.example.bank_service
+
+data class AuthenticationObject (
+    var id: String? = null,
+    var userPhone: String? = null,
+    var user: String? = null,
+    var key: String? = null
+)
