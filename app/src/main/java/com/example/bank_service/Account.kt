398cb19936e6cba1708922424183db5e37897ca8
hwaun123 <67179490+hwaun123@users.noreply.github.com>
2021-10-18 19:28:20 +0900

    [Add] Account Class
    Account Class

---
+++ app/src/main/java/com/example/bank_service/Account.kt
@@ -0,0 +1,5 @@
+package com.example.bank_service
+
+data class Account(
+
+)
---
+++ app/src/main/java/com/example/bank_service/AccountObject.kt
@@ -0,0 +1,9 @@
+package com.example.bank_service
+
+data class AccountObject(
+    var accountId: String? = null,
+    var money: String? = null,
+    var password: String? = null,
+    var userPhone: String? = null,
+    var user: String? = null
+)
