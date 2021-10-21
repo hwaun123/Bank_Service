40e0b750d4f5eca60da095ea299f9c0782e71f2c
hwaun123 <67179490+hwaun123@users.noreply.github.com>
2021-10-21 15:16:16 +0900

    [Add] EasyRegisterActivity
    EasyRegisterActivity

---
+++ app/src/main/java/com/example/bank_service/activity/EasyRegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.bank_service.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bank_service.R;
+
+public class EasyRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_easy_register);
+    }
+}
