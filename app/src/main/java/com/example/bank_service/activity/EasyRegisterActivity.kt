cc34a084ef952904146a3722b424f648c76c2205
hwaun123 <67179490+hwaun123@users.noreply.github.com>
2021-10-26 15:24:33 +0900

    [Fix] EasyRegisterActivity
    EasyRegisterActivity

---
+++ app/src/main/java/com/example/bank_service/activity/EasyRegisterActivity.kt
@@ -0,0 +1,14 @@
+package com.example.bank_service.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.bank_service.R
+
+class EasyRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_easy_register)
+
+        
+    }
+}
--- app/src/main/java/com/example/bank_service/activity/MainActivity.kt
+++ app/src/main/java/com/example/bank_service/activity/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.bank_service.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.example.bank_service.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imgUser: ImageView = findViewById(R.id.img_user)
+
+        imgUser.setOnClickListener {
+            val intent: Intent = Intent(this, EasyRegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
