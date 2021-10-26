214d26c0561e1ad444248e069c5e11ea6a0ab7f5
hwaun123 <67179490+hwaun123@users.noreply.github.com>
2021-10-26 15:35:20 +0900

    [Add] EasyRegister
    EasyRegister

--- app/src/main/java/com/example/bank_service/activity/EasyRegisterActivity.kt
+++ app/src/main/java/com/example/bank_service/activity/EasyRegisterActivity.kt
@@ -1,6 +1,10 @@
 package com.example.bank_service.activity
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.bank_service.R
 
@@ -9,6 +13,15 @@ class EasyRegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_easy_register)
 
-        
+        val easyRegisterBack: ImageView = findViewById(R.id.iv_easy_register_back)
+        easyRegisterBack.setOnClickListener { finish() }
+
+        val easyRegisterConfirm: Button = findViewById(R.id.btn_easy_register_confirm)
+        val editEasyRegister: EditText = findViewById(R.id.edit_easyregister)
+        easyRegisterConfirm.setOnClickListener{
+            var easyRegisterPw: String = editEasyRegister.text.toString()
+            Toast.makeText(this,easyRegisterPw,Toast.LENGTH_SHORT).show()
+            finish()
+        }
     }
 }
