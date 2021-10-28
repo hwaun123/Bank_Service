4b4effbd34dcc95a2566c0336cb0b67df0ba731c
hwaun123 <67179490+hwaun123@users.noreply.github.com>
2021-10-28 13:27:48 +0900

    [Add] Retrofit interface EasyRegister
    Retrofit interface EasyRegister

--- app/src/main/java/com/example/bank_service/RetrofitService.kt
+++ app/src/main/java/com/example/bank_service/RetrofitService.kt
@@ -3,6 +3,7 @@ package com.example.bank_service
 import com.example.bank_service.data_class.Login
 import com.example.bank_service.data_class.Register
 import com.example.bank_service.data_class.Userinfor
+import com.google.gson.annotations.SerializedName
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -17,5 +18,11 @@ interface RetrofitService {
     @POST("auth/login")
     fun login(
         @Body login: Login
+    ): Call<Userinfor?>
+
+    @POST("auth/easy/register")
+    fun easyRegister(
+        @Body
+        key: String
     ): Call<Userinfor?>
 }
